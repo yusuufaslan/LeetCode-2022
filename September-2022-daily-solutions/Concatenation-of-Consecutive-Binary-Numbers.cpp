58c6a5ecb051918ef3e3a359acc82bd8feb55977
Yusuf Aslan <yusuufaslan19@gmail.com>
2022-09-23 10:23:13 +0300

    descriptions of problems and 23sep were added

---
+++ September-2022-daily-solutions/Concatenation-of-Consecutive-Binary-Numbers.cpp
@@ -0,0 +1,18 @@
+/*
+    author: @yusuufaslan
+    Date: 23 September 2022
+    Problem number at LeetCode: 1680
+    Problem name: Concatenation of Consecutive Binary Numbers
+    level: medium
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+
+int main(int argc, char const *argv[])
+{
+    // code
+    return 0;
+}
