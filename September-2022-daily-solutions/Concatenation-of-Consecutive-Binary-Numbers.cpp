5523b7b12435ad464aff732ba1d39bb9b5294203
Yusuf Aslan <yusuufaslan19@gmail.com>
2022-09-23 10:53:27 +0300

    solution of sep23 was added.

--- September-2022-daily-solutions/Concatenation-of-Consecutive-Binary-Numbers.cpp
+++ September-2022-daily-solutions/Concatenation-of-Consecutive-Binary-Numbers.cpp
@@ -10,9 +10,33 @@
 #include <algorithm>
 #include <vector>
 #include <string>
+#include <math.h>
+
+using namespace std;
+
+long int MOD = 1e9 + 7;
+
+class Solution {
+public:
+
+    int numberOfBits(int n) {
+        return log2(n) + 1;
+    }
+
+    int concatenatedBinary(int n) {
+        long ans = 0;
+
+        for (int i = 1; i <= n; i++)
+            ans = ((ans << numberOfBits(i)) + i) % MOD;
+        
+        return ans;
+    }
+};
 
 int main(int argc, char const *argv[])
 {
-    // code
+    Solution s;
+    cout << s.concatenatedBinary(12) << endl;
+
     return 0;
 }
