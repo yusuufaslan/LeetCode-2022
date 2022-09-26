444bb37eba7a37ea2ab75251c696a7e2a5548d89
Yusuf Aslan <yusuufaslan19@gmail.com>
2022-09-26 21:11:14 +0300

    13-sep solution was added.

--- September-2022-daily-solutions/UTF-8-Validation.cpp
+++ September-2022-daily-solutions/UTF-8-Validation.cpp
@@ -16,8 +16,52 @@
 
 using namespace std;
 
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int num_of_bytes_to_process = 0;
+        for (int i = 0; i < data.size(); i++) {
+            string binary_string = bitset<8>(data[i]).to_string(); 
+
+            if (binary_string.length() >= 8){
+                binary_string = binary_string.substr(binary_string.length() - 8);
+            }
+            else {
+                string bin = "00000000";
+                binary_string = bin.substr(binary_string.length() % 8) + binary_string;
+            }
+
+            if (num_of_bytes_to_process == 0) {
+                for (int i = 0; i < binary_string.size(); i++) {
+                    if (binary_string[i] == '0')
+                        break;
+                    else 
+                        num_of_bytes_to_process++;
+                }
+
+                if (num_of_bytes_to_process == 0) {
+                    continue;
+                }
+
+                if (num_of_bytes_to_process > 4 || num_of_bytes_to_process == 1)
+                    return false;
+                
+            }
+            else {
+                if ( !(binary_string[0] == '1' && binary_string[1] == '0') )
+                    return false;
+            }
+
+            num_of_bytes_to_process--;
+        }
+        return num_of_bytes_to_process == 0;
+    }
+};
+
 int main(int argc, char const *argv[])
 {
-    /* code */
+    string y = "yusuf";
+    cout << y.substr(y.length() - 2) << endl;
+
     return 0;
 }
