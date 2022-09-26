aa7b52126b62460fce1fe53f4d9b590a8956805e
Yusuf Aslan <yusuufaslan19@gmail.com>
2022-09-26 20:26:09 +0300

    12-sep solution was added.

---
+++ September-2022-daily-solutions/Pseudo-Palindromic-Paths-in-a-Binary-Tree.cpp
@@ -0,0 +1,23 @@
+/*
+    author: @yusuufaslan
+    Date: 14 September 2022
+    Problem number at LeetCode: 1457
+    Problem name: Pseudo-Palindromic Paths in a Binary Tree
+    level: medium
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
---
+++ September-2022-daily-solutions/UTF-8-Validation.cpp
@@ -0,0 +1,23 @@
+/*
+    author: @yusuufaslan
+    Date: 13 September 2022
+    Problem number at LeetCode: 393
+    Problem name: UTF-8 Validation
+    level: medium
+*/
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <math.h>
+#include <bits/stdc++.h>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    return 0;
+}
